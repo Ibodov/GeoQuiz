880f9fe3cbaafdf3ff115f6232915998e813f131
Android3 <billing@babilon-t.com>
2021-04-07 18:10:44 +0500

    add Project to git

---
+++ app/src/main/java/ibodov/smile/geoquiz/QuesActivity.java
@@ -0,0 +1,14 @@
+package ibodov.smile.geoquiz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class QuesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+    }
+}
